784d588cded43ff63e9f724bab5a49e7a1866518
GuillermoCP07 <guill120600@gmail.com>
2022-11-20 19:54:34 -0600

    Página con la API del clima

---
+++ resources/js/clima.js
@@ -0,0 +1,85 @@
+window.addEventListener('load', ()=> {
+    let lon
+    let lat
+    const kelvin = 273.15
+
+
+    let temperaturaValor = document.getElementById('temperatura-valor')  
+    let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
+    
+    let ubicacion = document.getElementById('ubicacion')  
+    let iconoAnimado = document.getElementById('icono-animado') 
+
+    let vientoVelocidad = document.getElementById('viento-velocidad') 
+
+
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition( posicion => {
+           //console.log(posicion.coords.latitude)
+           lon = posicion.coords.longitude
+           lat = posicion.coords.latitude
+            //ubicación actual    
+           const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=56350e7b893b544cac45440e8b2e4b5c`
+
+           //ubicación por ciudad
+           //const url = `https://api.openweathermap.org/data/2.5/weather?q=Kanasin&lang=es&units=metric&appid=$56350e7b893b544cac45440e8b2e4b5c`
+
+           console.log(url)
+           /////////////////fetch(url)
+           /////////////////.then( response => { return response.json()})
+           /////////////////.then( datos => {
+            //console.log(datos.main.temp)
+           ///////////////// let temp = Math.round(datos.main.temp - kelvin)
+           ///////////////// temperaturaValor.textContent = `${temp} °C`
+            //console.log(datos.weather[0].description)
+           ///////////////// let desc = datos.weather[0].description
+           ///////////////// temperaturaDescripcion.textContent = desc.toUpperCase()
+            //console.log(datos.name)
+           ///////////////// ubicacion.textContent = datos.name
+           ///////////////// console.log(datos.wind.speed)
+           ///////////////// vientoVelocidad.textContent = `${datos.wind.speed} m/s`
+
+            //para iconos dinámicos
+            //////console.log(datos.weather[0].main)
+            //////switch (datos.weather[0].main) {
+              //////  case 'Thunderstorm':
+               //////   iconoAnimado.src='ani////mated/thunder.svg'
+               //////   console.log('TORMENTA');
+               //////   break;
+               ////// case 'Drizzle':
+               //////   iconoAnimado.src='animated/rainy-2.svg'
+                //////  console.log('LLOVIZNA');
+               //////   break;
+               ////// case 'Rain':
+                 //// iconoAnimado.src='animated/rainy-7.svg'
+                  ////console.log('LLUVIA');
+                 //// break;
+                ////case 'Snow':
+                 //// iconoAnimado.src='animated/snowy-6.svg'
+                 ////   console.log('NIEVE');
+                 //// break;                        
+                ////case 'Clear':
+                ////    iconoAnimado.src='animated/day.svg'
+                 ////   console.log('LIMPIO');
+                 //// break;
+                ////case 'Atmosphere':
+                 //// iconoAnimado.src='animated/weather.svg'
+                 ////   console.log('ATMOSFERA');
+                 ////   break;  
+                ////case 'Clouds':
+                  ////  iconoAnimado.src='animated/cloudy-day-1.svg'
+                  ////  console.log('NUBES');
+                  ////  break;  
+                ////default:
+                 //// iconoAnimado.src='animated/cloudy-day-1.svg'
+                 //// console.log('por defecto');
+              ////}
+           ////})
+           ////.catch( error => {
+           //// console.log(error)
+           ////})
+       })
+          
+    }
+})
+/////////////////
--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -42,6 +42,92 @@
 }
     </script>
 
+    <script>
+      window.addEventListener('load', ()=> {
+    let lon
+    let lat
+    const kelvin = 273.15
+
+
+    let temperaturaValor = document.getElementById('temperatura-valor')  
+    let temperaturaDescripcion = document.getElementById('temperatura-descripcion')  
+    
+    let ubicacion = document.getElementById('ubicacion')  
+    let iconoAnimado = document.getElementById('icono-animado') 
+
+    let vientoVelocidad = document.getElementById('viento-velocidad') 
+
+
+    if(navigator.geolocation){
+       navigator.geolocation.getCurrentPosition( posicion => {
+           //console.log(posicion.coords.latitude)
+           lon = posicion.coords.longitude
+           lat = posicion.coords.latitude
+            //ubicación actual    
+           const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=56350e7b893b544cac45440e8b2e4b5c`
+
+           //ubicación por ciudad
+           //const url = `https://api.openweathermap.org/data/2.5/weather?q=Kanasin&lang=es&units=metric&appid=$56350e7b893b544cac45440e8b2e4b5c`
+
+           //console.log(url)
+           fetch(url)
+           .then( response => { return response.json()})
+           .then( datos => {
+            //console.log(datos.main.temp)
+            let temp = Math.round(datos.main.temp - kelvin)
+            temperaturaValor.textContent = `${temp} °C`
+            //console.log(datos.weather[0].description)
+            let desc = datos.weather[0].description
+            temperaturaDescripcion.textContent = desc.toUpperCase()
+            //console.log(datos.name)
+            ubicacion.textContent = datos.name
+           // console.log(datos.wind.speed)
+            vientoVelocidad.textContent = `${datos.wind.speed} m/s`
+
+            //para iconos dinámicos
+            console.log(datos.weather[0].main)
+            switch (datos.weather[0].main) {
+                case 'Thunderstorm':
+                  iconoAnimado.src='ani////mated/thunder.svg'
+                  console.log('TORMENTA');
+                  break;
+                case 'Drizzle':
+                  iconoAnimado.src='animated/rainy-2.svg'
+                  console.log('LLOVIZNA');
+                  break;
+                case 'Rain':
+                  iconoAnimado.src='animated/rainy-7.svg'
+                  console.log('LLUVIA');
+                  break;
+                case 'Snow':
+                  iconoAnimado.src='animated/snowy-6.svg'
+                    console.log('NIEVE');
+                  break;                        
+                case 'Clear':
+                    iconoAnimado.src='animated/day.svg'
+                    console.log('LIMPIO');
+                  break;
+                case 'Atmosphere':
+                   iconoAnimado.src='animated/weather.svg'
+                    console.log('ATMOSFERA');
+                    break;  
+                case 'Clouds':
+                    iconoAnimado.src='animated/cloudy-day-1.svg'
+                    console.log('NUBES');
+                    break;  
+                default:
+                  iconoAnimado.src='animated/cloudy-day-1.svg'
+                  console.log('por defecto');
+              }
+           })
+           .catch( error => {
+            console.log(error)
+           })
+       })
+          
+    }
+})
+    </script>
   </head>
   <body>
     <i class="ir_arriba fa-solid fa-circle-arrow-up"></i>
@@ -63,6 +149,19 @@
         <picture>
           <img src="imagenes/logocabecera.jpg" alt="logo_spa_lupita" />
         </picture>
+        <div class="contenedor_clima">
+        <div id="caja1">
+            <h1 id="temperatura-valor"></h1>
+            <h1 id="temperatura-descripcion"></h1>
+        </div>
+        <div id="caja2">
+            <h2 id="ubicacion"></h2>
+        </div>
+        <div id="caja3">
+            <h3>Veloc. del Viento</h3>
+            <h1 id="viento-velocidad"></h1>
+        </div>
+    </div>
         <aside class="iconoredes">
           <a class="uno" href="https://www.facebook.com"
             ><i class="fa-brands fa-facebook-square"></i
@@ -73,6 +172,7 @@
           <a class="tres" href="https://www.twitter.com"
             ><i class="fa-brands fa-twitter-square"></i
           ></a>
+          <a id="iniciospa" href="{{ url('usuario/')}}">Iniciar Sesion</a>
         </aside>
       </div>
       <nav class="ancho">
